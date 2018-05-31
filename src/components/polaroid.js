da3f63debdb6831d3f37eb640c01ab85d099d6e5
Benedikt Rötsch <benedikt@contentful.com>
2018-05-31 15:27:55 +0200

    replace sqip with traced svgs

--- src/components/polaroid.js
+++ src/components/polaroid.js
@@ -59,24 +59,12 @@ function generateDynamicStyle(imageData) {
 const Polaroid = ({ image }) => {
   const imageData = data[image.name]
 
-  return (
-    <a
-      href={imageData.url}
-      title={`by ${imageData.author}`}
-      className="polaroid"
-      style={generateDynamicStyle(imageData)}
-    >
+  return <a href={imageData.url} title={`by ${imageData.author}`} className="polaroid" style={generateDynamicStyle(imageData)}>
       <div className="polaroid-image-wrapper">
-        <Image
-          sizes={{
-            ...image.childImageSharp.sizes,
-            base64: image.childImageSharp.sqip.dataURI,
-          }}
-        />
+        <Image sizes={image.childImageSharp.sizes} />
       </div>
       <div className="polaroid-author">{`📷 ${imageData.author}`}</div>
     </a>
-  )
 }
 
 Polaroid.propTypes = {
