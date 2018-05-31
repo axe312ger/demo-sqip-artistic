d9fc394e055851f22c2297fa8b11d638588aea09
Benedikt Rötsch <benedikt@contentful.com>
2018-05-31 15:27:55 +0200

    replace sqip with traced svgs

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,28 +11,14 @@ const Layout = ({ children, data }) => {
     <Polaroid image={image.node} key={image.node.name} />
   ))
   const background = data.background.edges[0].node
-  return (
-    <div>
-      <div
-        style={{
-          position: `relative`,
-          height: `66vw`,
-          zIndex: 0,
-        }}
-      >
+  return <div>
+      <div style={{ position: `relative`, height: `66vw`, zIndex: 0 }}>
         <Image
-          // Inject the sqip dataURI as base64 value
-          sizes={{
-            ...background.childImageSharp.sizes,
-            base64: background.childImageSharp.sqip.dataURI,
-          }}
-          alt={background.name}
-        />
+          sizes={background.childImageSharp.sizes} alt={background.name} />
         <div>{images}</div>
       </div>
       <div style={{ padding: `5vw` }}>{children()}</div>
     </div>
-  )
 }
 
 Layout.propTypes = {
@@ -43,50 +29,32 @@ Layout.propTypes = {
 export default Layout
 
 export const query = graphql`
-  query SiteTitleQuery {
-    images: allFile(
-      filter: { sourceInstanceName: { eq: "images" }, ext: { eq: ".jpg" } }
-    ) {
-      edges {
-        node {
-          publicURL
-          name
-          childImageSharp {
-            sizes(maxWidth: 400, maxHeight: 400) {
-              ...GatsbyImageSharpSizes_noBase64
-            }
-            sqip(
-              # Make sure to keep the same aspect ratio when cropping
-              # With 256px as maximum dimension is the perfect value to speed up the process
-              width: 256
-              height: 256
-              numberOfPrimitives: 15
-              blur: 8
-              mode: 1
-            ) {
-              dataURI
-            }
-          }
-        }
-      }
-    }
-    background: allFile(
-      filter: { sourceInstanceName: { eq: "background" }, ext: { eq: ".jpg" } }
-    ) {
-      edges {
-        node {
-          publicURL
-          name
-          childImageSharp {
-            sizes(maxWidth: 4000) {
-              ...GatsbyImageSharpSizes_noBase64
-            }
-            sqip(numberOfPrimitives: 160, blur: 0) {
-              dataURI
-            }
-          }
-        }
-      }
-    }
-  }
-`
+         query SiteTitleQuery {
+           images: allFile(filter: { sourceInstanceName: { eq: "images" }, ext: { eq: ".jpg" } }) {
+             edges {
+               node {
+                 publicURL
+                 name
+                 childImageSharp {
+                   sizes(maxWidth: 400, maxHeight: 400) {
+                     ...GatsbyImageSharpSizes_tracedSVG
+                   }
+                 }
+               }
+             }
+           }
+           background: allFile(filter: { sourceInstanceName: { eq: "background" }, ext: { eq: ".jpg" } }) {
+             edges {
+               node {
+                 publicURL
+                 name
+                 childImageSharp {
+                   sizes(maxWidth: 4000) {
+                     ...GatsbyImageSharpSizes_tracedSVG
+                   }
+                 }
+               }
+             }
+           }
+         }
+       `
